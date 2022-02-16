3f4fb23a4f3e49c66011b28fce2d8ac5d38ce3cc
Andrea <andrea.franco3@icloud.com>
2022-02-16 14:13:39 +0100

    inizio progetto
    lettura dati

---
+++ FrancoProvaComune5/FrancoProvaComune5/Maratona.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrancoProvaComune5
+{
+    class Maratona
+    {
+        public string nomeAtleta { get; set; }
+
+        public string societàDiAppartenenza { get; set; }
+
+        public string tempoImpiegato { get; set; }
+
+        public string cittàMaratona { get; set; }
+    }
+}
---
+++ FrancoProvaComune5/FrancoProvaComune5/Maratone.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FrancoProvaComune5
+{
+    class Maratone
+    {
+        public List<Maratona> elencoMaratone;
+
+
+        public Maratone()
+        {
+            elencoMaratone = new List<Maratona>();
+        }
+
+        public void Aggiungi(Maratona maratona)
+        {
+            elencoMaratone.Add(maratona);
+        }
+
+        public void LeggiDati()
+        {
+            using (FileStream flussoDati = new FileStream("maratona.txt", FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader lettoreDati = new StreamReader(flussoDati))
+                {
+                    while (!lettoreDati.EndOfStream)
+                    {
+                        string linea = lettoreDati.ReadLine();
+                        string[] campi = linea.Split('%');
+
+                        Maratona maratona = new Maratona();
+                        maratona.nomeAtleta = campi[0];
+                        maratona.societàDiAppartenenza = campi[1];
+                        maratona.tempoImpiegato = campi[2];
+                        maratona.cittàMaratona = campi[3];
+
+                        Aggiungi(maratona);
+
+
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+   
+
+                
